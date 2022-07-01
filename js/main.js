40a1f33624cac4aea041c3861f559fbe7098276c
nugrahaadis12 <107617174+nugrahaadis12@users.noreply.github.com>
2022-07-01 10:35:44 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,78 @@
+// test
+let pesan;
+pesan = "hello";
+pesan1 = "world";
+console.log(pesan, pesan1);
+
+// test 2
+console.log(6 / 10); 
+
+// test 3
+let halo = "Halo Dunia";
+let pesan2;
+
+/* Copy nilai 'halo dunia' dari halo ke pesan */
+pesan2 = halo;
+console.log(pesan2);
+
+// test 4
+let nama = "sabrina", 
+umur = 23, 
+jenis = "perempuan";
+
+console.log(nama);
+console.log(umur);
+console.log(jenis);
+
+// mempelajarin contant variable
+
+// const test 1
+const bumi = "bulan";
+
+console.log(bumi);
+
+// const test 2
+
+const name = "Sabrina";
+const pekerjaan = "Fasilitator Binar";
+
+console.log('hallo, nama ${name}, aku kerja sebagai ${pekerjaan}');
+
+let a = 1, b = 1;
+
+// const test quizz
+let c = ++a;
+let d = b++;
+
+console.log(a,b,c,d);
+
+
+// const modify and assign
+
+let n = 2;
+n *= 3 + 5;
+
+console.log(n);
+
+
+
+// const modify and assign test 4
+
+let testA = 2;
+let testB = 1 + (testA *= 2);
+
+console.log(testA , testB);
+
+// operatir OR
+
+// operator OR test 1
+
+let hasil = false || 10;
+console.log(hasil);
+
+console.log(true);
+
+
+let h = 1 && null && 2
+
+consol (1 && null && 2);
